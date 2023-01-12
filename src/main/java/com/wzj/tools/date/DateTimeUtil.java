30d35ef1ede5c89c31789d84a976b87d317906d0
ZhiJian-Wang <zhijian.wang.work@outlook.com>
2023-01-12 16:05:06 +0800

    update DateTimeUtil

--- src/main/java/com/wzj/tools/date/DateTimeUtil.java
+++ src/main/java/com/wzj/tools/date/DateTimeUtil.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -22,18 +25,19 @@ public class DateTimeUtil {
     private static final String YYYY_MM_DD="yyyy-MM-dd";
     private static final String HH_MM_SS="HH:mm:ss";
 
-    private static SimpleDateFormat getSimpleDateFormat(String formatStr){
-        if (formatStr==null||"".equals(formatStr)){
-            formatStr=YYYY_MM_DD_HH_MM_SS;
-        }
-        return new SimpleDateFormat(formatStr);
-    }
-
     private static DateTimeFormatter getDateTimeFormatter(String formatStr){
         if (formatStr==null||"".equals(formatStr)){
             formatStr=YYYY_MM_DD_HH_MM_SS;
         }
         return DateTimeFormatter.ofPattern(formatStr);
+    }
+
+    private static LocalDateTime getLocalDateTime(Date date){
+        return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+    }
+
+    private static Date getDate(LocalDateTime localDateTime){
+        return Date.from(localDateTime.atZone( ZoneId.systemDefault()).toInstant());
     }
 
     /**
@@ -42,14 +46,11 @@ public class DateTimeUtil {
      * @param formatStr 格式字符串
      * @return 格式化后字符串时间
      */
-    public static String formatDateTimeToString(Date date,String formatStr) {
-        SimpleDateFormat simpleDateFormat = getSimpleDateFormat(formatStr);
+    public static String formatDateToString(Date date,String formatStr) {
         String dateStr = null;
-        try {
-            dateStr = simpleDateFormat.format(date);
-        } catch (Exception e) {
-            logger.error(e.getMessage(),e);
-            throw new RuntimeException(e);
+        if (date!=null){
+            LocalDateTime localDateTime=getLocalDateTime(date);
+            dateStr=formatDateTimeToString(localDateTime,formatStr);
         }
         return dateStr;
     }
@@ -63,12 +64,43 @@ public class DateTimeUtil {
     public static String formatDateTimeToString(LocalDateTime localDateTime, String formatStr) {
         DateTimeFormatter dateTimeFormatter = getDateTimeFormatter(formatStr);
         String dateStr = null;
-        try {
-            dateStr = dateTimeFormatter.format(localDateTime);
-        } catch (Exception e) {
-            logger.error(e.getMessage(),e);
-            throw new RuntimeException(e);
+        if (localDateTime!=null){
+            try {
+                dateStr = dateTimeFormatter.format(localDateTime);
+            } catch (Exception e) {
+                logger.error(e.getMessage(),e);
+                throw new RuntimeException(e);
+            }
         }
         return dateStr;
     }
+
+    public static Date getDateOfFirst(){
+        return getDate(getDateTimeOfFirst());
+    }
+
+    public static Date getDateOfFirst(int month){
+        return getDate(getDateTimeOfFirst(month));
+    }
+
+    public static LocalDateTime getDateTimeOfFirst(){
+        LocalDate localDate=LocalDate.now();
+        localDate=localDate.withDayOfMonth(1);
+        return localDate.atStartOfDay();
+    }
+
+    public static LocalDateTime getDateTimeOfFirst(int month){
+        LocalDate localDate=LocalDate.now();
+        localDate=localDate.plusMonths(month).withDayOfMonth(1);
+        return localDate.atStartOfDay();
+    }
+
+    public static String getDateTimeStringOfFirst(){
+        return formatDateTimeToString(getDateTimeOfFirst(),null);
+    }
+
+    public static String getDateTimeStringOfFirst(int month){
+        return formatDateTimeToString(getDateTimeOfFirst(month),null);
+    }
+
 }
